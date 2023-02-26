ec50e918914dcc5ed9a869e0806108dff568d8c9
tkuhnke <kuhnktho@googlemail.com>
2023-02-26 22:51:27 +0100

    * clean up

--- semantic-analyzer/src/main/java/io/guw/knxutils/semanticanalyzer/semanticmodel/util/SemanticIdentifier.java
+++ semantic-analyzer/src/main/java/io/guw/knxutils/semanticanalyzer/semanticmodel/util/SemanticIdentifier.java
@@ -4,7 +4,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface KnxIdentifier {
+public @interface SemanticIdentifier {
     String[] terms() default {};
 
     String[] prefixes() default {};
