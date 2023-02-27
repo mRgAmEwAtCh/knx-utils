dd9f99b902919e6aa06d02321109ce4cf8f2b456
Thomas Kuhnke <thomas.kuhnke@mercedes-benz.com>
2023-02-27 19:13:19 +0100

    * test annotation processor

--- semantic-analyzer/src/main/java/io/guw/knxutils/semanticanalyzer/semanticmodel/util/SemanticIdentifier.java
+++ semantic-analyzer/src/main/java/io/guw/knxutils/semanticanalyzer/semanticmodel/util/SemanticIdentifier.java
@@ -1,8 +1,11 @@
 package io.guw.knxutils.semanticanalyzer.semanticmodel.util;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SemanticIdentifier {
     String[] terms() default {};
